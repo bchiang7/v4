11a6f71d0c7f928386bb24e1cd7dcbce7dae8d21
bchiang7 <brittany.chiang@gmail.com>
2018-08-16 17:15:31 -0400

    Get tabs working with state and props

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styled from 'styled-components';
-import { theme, mixins, Section, H3, A, P } from '../style';
+import { theme, Section, H3, A, P } from '../style';
 
 const JobsContainer = Section.extend`
   position: relative;
@@ -25,33 +25,22 @@ const Tab = A.extend`
   text-align: center;
   transition: ${theme.transition};
   border-left: 2px solid ${theme.colors.darkGrey};
-  color: ${theme.colors.grey};
+  border-radius: ${theme.borderRadius};
   font-family: ${theme.fonts.SFMono};
   font-size: ${theme.fontSizes.small};
-
-  &:hover,
-  &.active {
+  color: ${props => (props.isActive ? theme.colors.green : theme.colors.grey)};
+  &:hover {
     color: ${theme.colors.green};
   }
-
   &:focus {
-    background-color: ${theme.colors.darkNavy};
+    background-color: ${theme.colors.lightNavy};
   }
-
   a:first-of-type {
     border-top-left-radius: ${theme.borderRadius};
   }
   a:last-of-type {
     border-top-right-radius: ${theme.borderRadius};
   }
-
-  /* @for $i from 1 through 3 {
-    &.tab-#{$i}.active {
-      & ~ .highlighter {
-        transform: translateY(#{($i * ${theme.tabHeight}) - ${theme.tabHeight}});
-      }
-    }
-  } */
 `;
 const Highlighter = styled.span`
   display: block;
@@ -61,80 +50,80 @@ const Highlighter = styled.span`
   position: absolute;
   top: 0;
   left: 0;
-  transform: translateY(0);
   transition: ${theme.transition};
   transition-delay: 0.1s;
   z-index: 10;
+  transform: translateY(
+    ${props => (props.selectedTabId > 1 ? props.selectedTabId * 40 - 40 : 0)}px
+  );
 `;
-const TabContentContainer = styled.div`
+const ContentContainer = styled.div`
   position: relative;
   padding: 0 ${theme.margin};
   flex-grow: 1;
 `;
 const TabContent = styled.div`
-  position: absolute;
   top: 0;
   left: 0;
-  opacity: 0;
-  z-index: 0;
-  transition: opacity 0s ease-in-out;
-
-  &.visible {
-    position: relative;
-    opacity: 1;
-    width: 100%;
-    height: auto;
-    transition: opacity 0.5s ease-in-out;
-    z-index: 2;
-  }
+  width: 100%;
+  height: auto;
+  opacity: ${props => (props.isActive ? 1 : 0)};
+  z-index: ${props => (props.isActive ? 2 : 0)};
+  position: ${props => (props.isActive ? 'relative' : 'absolute')};
+  transition: opacity ${props => (props.isActive ? '0.5s' : '0s')} ease-in-out;
 `;
 
-const Jobs = () => (
-  <JobsContainer>
-    <H3>Where I've Worked</H3>
-    <TabsContainer>
-      <Tabs>
-        <Tab className="tab tab-1 active" href="#" data-tab="1">
-          Tab 1
-        </Tab>
-        <Tab className="tab tab-2" href="#" data-tab="2">
-          Tab 2
-        </Tab>
-        <Tab className="tab tab-3" href="#" data-tab="3">
-          Tab 3
-        </Tab>
-        <Highlighter />
-      </Tabs>
-      <TabContentContainer>
-        <TabContent data-tab="1">
-          <p>
-            Lorem ipsum dolor amet umami kogi you probably haven't heard of them activated charcoal
-            hexagon literally. Vice art party XOXO retro blue bottle. Squid tousled skateboard man
-            bun pabst unicorn sriracha poutine echo park crucifix plaid health goth four dollar
-            toast. XOXO gochujang PBR&B kombucha brooklyn street art lyft swag.
-          </p>
-        </TabContent>
-        <TabContent data-tab="2">
-          <p>
-            Wayfarers small batch brooklyn, pabst godard flannel blog authentic fingerstache salvia
-            disrupt kombucha pickled. Actually hammock semiotics DIY cold-pressed lo-fi swag franzen
-            chia locavore. Biodiesel venmo irony, salvia shaman plaid photo booth keffiyeh VHS
-            slow-carb chillwave knausgaard vape occupy.
-          </p>
-        </TabContent>
+const tabData = [
+  { id: 1, name: 'Tab 1', text: 'text' },
+  { id: 2, name: 'Tab 2', text: 'text-2' },
+  { id: 3, name: 'Tab 3', text: 'text-3' },
+];
 
-        <TabContent data-tab="3">
-          <p>
-            Gluten-free you probably haven't heard of them jianbing pitchfork pabst. Kombucha occupy
-            iPhone live-edge beard iceland freegan small batch before they sold out DIY marfa
-            chillwave sustainable taiyaki bushwick. Meggings franzen kogi, next level fingerstache
-            +1 kitsch YOLO activated charcoal craft beer adaptogen twee. Yuccie tumeric irony
-            heirloom, shoreditch kinfolk swag cold-pressed 90's chicharrones.
-          </p>
-        </TabContent>
-      </TabContentContainer>
-    </TabsContainer>
-  </JobsContainer>
-);
+class Jobs extends Component {
+  state = {
+    selectedTabId: 1,
+  };
+
+  isActive = id => this.state.selectedTabId === id;
+
+  setActiveTab = (selectedTabId, e) => {
+    e.preventDefault();
+    this.setState({ selectedTabId });
+  };
+
+  render() {
+    const { selectedTabId } = this.state;
+
+    return (
+      <JobsContainer>
+        <H3>Where I've Worked</H3>
+        <TabsContainer>
+          <Tabs>
+            {tabData &&
+              tabData.map(tab => (
+                <Tab
+                  href="#"
+                  key={tab.id}
+                  content={tab.text}
+                  isActive={this.isActive(tab.id)}
+                  onClick={e => this.setActiveTab(tab.id, e)}>
+                  <span>{tab.name}</span>
+                </Tab>
+              ))}
+            <Highlighter selectedTabId={selectedTabId} />
+          </Tabs>
+          <ContentContainer>
+            {tabData &&
+              tabData.map(tab => (
+                <TabContent key={tab.id} isActive={this.isActive(tab.id)}>
+                  <P>{tab.text}</P>
+                </TabContent>
+              ))}
+          </ContentContainer>
+        </TabsContainer>
+      </JobsContainer>
+    );
+  }
+}
 
 export default Jobs;
--- src/style/Section.js
+++ src/style/Section.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Section = styled.section`
   margin: 0 auto;
-  padding: 100px 0;
+  padding: 150px 0;
   max-width: 1100px;
 `;
 
