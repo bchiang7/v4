2728456407607577178e3d9809b9f799fe4f6a50
bchiang7 <brittany.chiang@gmail.com>
2018-10-03 12:22:31 -0400

    Update og images and fix meta tags

--- src/components/head.js
+++ src/components/head.js
@@ -21,33 +21,33 @@ import msIcon144x144 from '../../static/favicons/ms-icon-144x144.png';
 
 const Head = ({ metaData }) => (
   <Helmet>
-    <html lang="en" />
+    <html lang="en" prefix="og: http://ogp.me/ns#" />
     <title itemProp="name" lang="en">
       {metaData.title}
     </title>
     <link rel="icon" type="image/png" href={logo} />
-    <meta name="metaData.description" content={metaData.description} />
+    <meta name="description" content={metaData.description} />
     <meta name="keywords" content={config.siteKeywords} />
     <meta name="google-site-verification" content={config.googleVerification} />
-    <meta property="og:metaData.title" content={metaData.title} />
-    <meta property="og:metaData.description" content={metaData.description} />
+    <meta property="og:title" content={metaData.title} />
+    <meta property="og:description" content={metaData.description} />
     <meta property="og:type" content="website" />
     <meta property="og:url" content={metaData.siteUrl} />
     <meta property="og:site_name" content={metaData.title} />
     <meta property="og:image" content={config.siteImage} />
-    <meta property="og:image:width" content="1280" />
-    <meta property="og:image:height" content="800" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:locale" content={config.siteLanguage} />
     <meta itemProp="name" content={metaData.title} />
-    <meta itemProp="metaData.description" content={metaData.description} />
+    <meta itemProp="description" content={metaData.description} />
     <meta itemProp="image" content={config.siteImage} />
-    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:card" content="summary" />
     <meta name="twitter:url" content={metaData.siteUrl} />
     <meta name="twitter:site" content={config.twitterHandle} />
     <meta name="twitter:creator" content={config.twitterHandle} />
-    <meta name="twitter:metaData.title" content={metaData.title} />
-    <meta name="twitter:metaData.description" content={metaData.description} />
+    <meta name="twitter:title" content={metaData.title} />
+    <meta name="twitter:description" content={metaData.description} />
     <meta name="twitter:image:src" content={config.siteImage} />
     <meta name="twitter:image:alt" content={metaData.title} />
 
