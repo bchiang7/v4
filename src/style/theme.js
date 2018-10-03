ac15bdfe1b705a16871c680e47e69d193dad0941
bchiang7 <brittany.chiang@gmail.com>
2018-10-03 12:52:59 -0400

    Update sans-serif and monospace fallbacks

--- src/style/theme.js
+++ src/style/theme.js
@@ -29,8 +29,8 @@ const theme = {
 
   fonts: {
     Calibre:
-      'Calibre, system, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif',
-    SFMono: 'SF Mono, Fira Code, Fira Mono, monospace',
+      'Calibre, San Francisco, SF Pro Text, -apple-system, system-ui, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif',
+    SFMono: 'SF Mono, Fira Code, Fira Mono, Roboto Mono, Lucida Console, Monaco, monospace',
   },
 
   fontSizes: {
