c3d0722c508300aee849de9671a59440623b2bf0
Brittany Chiang <brittany.chiang@gmail.com>
2019-02-18 23:46:39 -0500

    Update copy

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Front-End Software Engineer',
+  siteTitle: 'Brittany Chiang | Web Developer',
   siteDescription:
-    'Web developer based in Boston, MA who specializes in developing (and occasionally designing) exceptional, high-quality websites and applications.',
+    'Brittany Chiang is a web developer based in Boston, MA who specializes in developing (and occasionally designing) exceptional, high-quality websites and applications.',
   siteKeywords:
     'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
   siteUrl: 'https://brittanychiang.com',
