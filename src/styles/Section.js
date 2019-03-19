addbb0958230b37cb6520eba7d8248dddde7c05e
Mike Laderman <mlader@brandeis.edu>
2019-03-18 20:22:54 -0400

    Reduce section padding

--- src/styles/Section.js
+++ src/styles/Section.js
@@ -3,7 +3,7 @@ import media from './media';
 
 const Section = styled.section`
   margin: 0 auto;
-  padding: 150px 0;
+  padding: 100px 0;
   max-width: 1000px;
 
   ${media.tablet`padding: 100px 0;`};
