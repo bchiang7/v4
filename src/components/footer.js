8e540dd7a7c6c98ecd579a9790e224e8813339eb
Dennis Trautwein <dennis.trautwein@freiheit.com>
2019-06-08 22:21:36 +0200

    Remove stars and fork count

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,6 @@ import {
   IconCodepen,
   IconInstagram,
   IconTwitter,
-  IconStar,
-  IconFork,
 } from '@components/icons';
 import { socialMedia } from '@config';
 import styled from 'styled-components';
@@ -51,23 +49,7 @@ const Copy = styled.div`
 const GithubLink = styled.a`
   color: ${colors.slate};
 `;
-const GithubInfo = styled.div`
-  margin-top: 10px;
-
-  & > span {
-    display: inline-flex;
-    align-items: center;
-    margin: 0 7px;
-  }
-  svg {
-    display: inline-block;
-    height: 15px;
-    width: auto;
-    margin-right: 5px;
-  }
-`;
-
-const Footer = ({ githubInfo }) => (
+const Footer = () => (
   <FooterContainer>
     <SocialContainer>
       <SocialItemList>
@@ -102,20 +84,7 @@ const Footer = ({ githubInfo }) => (
         href="https://github.com/bchiang7/v4"
         target="_blank"
         rel="nofollow noopener noreferrer">
-        <div>Designed &amp; Built by Brittany Chiang</div>
-
-        {githubInfo.stars && githubInfo.forks && (
-          <GithubInfo>
-            <span>
-              <IconStar />
-              <span>{githubInfo.stars}</span>
-            </span>
-            <span>
-              <IconFork />
-              <span>{githubInfo.forks}</span>
-            </span>
-          </GithubInfo>
-        )}
+        <div>Loud Shoutout to Brittany Chiang who Designed &amp; originally Built this website</div>
       </GithubLink>
     </Copy>
   </FooterContainer>
