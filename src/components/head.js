0093cf66e840d168f3a49322dbb55046f7bc5f8a
Dennis Trautwein <dennis.trautwein@freiheit.com>
2019-06-08 19:13:08 +0200

    Exchange favicons

--- src/components/head.js
+++ src/components/head.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import config from '@config';
-import favicon from '@images/favicons/favicon.ico';
+// import favicon from '@images/favicons/favicon.ico';
 import ogImage from '@images/og.png';
 import appleIcon57x57 from '@images/favicons/apple-icon-57x57.png';
 import appleIcon60x60 from '@images/favicons/apple-icon-60x60.png';
@@ -25,7 +25,7 @@ const Head = ({ metadata }) => (
     <title itemProp="name" lang="en">
       {metadata.title}
     </title>
-    <link rel="shortcut icon" href={favicon} />
+    {/* <link rel="shortcut icon" href={favicon} /> */}
     <meta name="description" content={metadata.description} />
     <meta name="keywords" content={config.siteKeywords} />
     <meta name="google-site-verification" content={config.googleVerification} />
