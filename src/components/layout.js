5a199fcb971f4b1dc4b07fcd2b4984cc7ab1b343
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-14 20:39:22 -0400

    Add require back in to layout for smooth scrolling

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,12 @@ import styled from 'styled-components';
 import { GlobalStyle, theme } from '@styles';
 const { colors, fontSizes, fonts } = theme;
 
+// https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
+if (typeof window !== 'undefined') {
+  // eslint-disable-next-line global-require
+  require('smooth-scroll')('a[href*="#"]');
+}
+
 const SkipToContent = styled.a`
   position: absolute;
   top: auto;
