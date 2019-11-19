d97034e5de0315093c03ec38dd0e201223b1edec
Brittany Chiang <brittany.chiang@gmail.com>
2019-11-19 01:02:23 -0500

    Move projects out of folders

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import { Layout } from '@components';
+import { IconGithub, IconExternal } from '@components/icons';
 import styled from 'styled-components';
 import { theme, media, Main } from '@styles';
 const { colors, fonts, fontSizes } = theme;
@@ -21,7 +23,7 @@ const StyledTitle = styled.h1`
   ${media.phablet`font-size: 50px;`};
   ${media.phone`font-size: 40px;`};
 `;
-const StyledSubtitle = styled.h2`
+const StyledSubtitle = styled.p`
   color: ${colors.green};
   margin: 0 0 20px 3px;
   font-size: ${fontSizes.medium};
@@ -30,20 +32,97 @@ const StyledSubtitle = styled.h2`
   ${media.desktop`font-size: ${fontSizes.small};`};
   ${media.tablet`font-size: ${fontSizes.smallish};`};
 `;
+const StyledList = styled.ul`
+  margin-top: 100px;
 
-const ArchivePage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer>
-      <StyledTitleContainer>
-        <StyledTitle>Archive</StyledTitle>
-        <StyledSubtitle>A big list of things I’ve worked on</StyledSubtitle>
-      </StyledTitleContainer>
-    </StyledMainContainer>
-  </Layout>
-);
+  li {
+    margin-bottom: 50px;
+  }
+`;
 
+const ArchivePage = ({ location, data }) => {
+  const projects = data.projects.edges;
+  console.log('📣: ArchivePage -> projects', projects);
+
+  return (
+    <Layout location={location}>
+      <StyledMainContainer>
+        <StyledTitleContainer>
+          <StyledTitle>Archive</StyledTitle>
+          <StyledSubtitle>A big list of things I’ve worked on</StyledSubtitle>
+          <StyledList>
+            {projects.length > 0 &&
+              projects.map(({ node }, i) => {
+                const { frontmatter, html } = node;
+                const { date, github, external, title, tech } = frontmatter;
+                return (
+                  <li key={i}>
+                    <h1>{`${new Date(date).getFullYear()}`}</h1>
+                    <h2>{title}</h2>
+                    <p dangerouslySetInnerHTML={{ __html: html }} />
+
+                    <div>
+                      {tech.map((tech, i) => (
+                        <span key={i}>{tech} &bull; </span>
+                      ))}
+                    </div>
+
+                    {github && (
+                      <a
+                        href={github}
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                        aria-label="Github Link">
+                        {/* <IconGithub /> */}
+                        GitHub Link
+                      </a>
+                    )}
+                    {external && (
+                      <a
+                        href={external}
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                        aria-label="External Link">
+                        {/* <IconExternal /> */}
+                        Live Site Link
+                      </a>
+                    )}
+                  </li>
+                );
+              })}
+          </StyledList>
+        </StyledTitleContainer>
+      </StyledMainContainer>
+    </Layout>
+  );
+};
 ArchivePage.propTypes = {
   location: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
 };
 
 export default ArchivePage;
+
+export const pageQuery = graphql`
+  {
+    projects: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/projects/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date
+            title
+            image
+            tech
+            github
+            external
+            show
+          }
+          html
+        }
+      }
+    }
+  }
+`;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -43,6 +43,15 @@ const theme = {
     h3: '32px',
   },
 
+  // todo: add spacing
+  spacing: {
+    xs: '5px',
+    sm: '10px',
+    md: '20px',
+    lg: '50px',
+    xl: '100px',
+  },
+
   easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
   transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
 
