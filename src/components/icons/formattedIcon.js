42f16a76308b21348cc7ff2b81d5a1c8f4259b2d
Brittany Chiang <brittany.chiang@gmail.com>
2020-03-07 19:42:55 -0500

    Use FormattedIcon component more

--- src/components/icons/formattedIcon.js
+++ src/components/icons/formattedIcon.js
@@ -1,34 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  IconGitHub,
-  IconExternal,
-  IconLinkedin,
-  IconCodepen,
-  IconInstagram,
-  IconTwitter,
   IconAppStore,
+  IconCodepen,
+  IconExternal,
+  IconFolder,
+  IconFork,
+  IconGitHub,
+  IconInstagram,
+  IconLinkedin,
+  IconLoader,
+  IconLocation,
+  IconLogo,
   IconPlayStore,
+  IconStar,
+  IconTwitter,
+  IconZap,
 } from '@components/icons';
 
 const FormattedIcon = ({ name }) => {
   switch (name) {
-    case 'GitHub':
-      return <IconGitHub />;
-    case 'Linkedin':
-      return <IconLinkedin />;
-    case 'Codepen':
-      return <IconCodepen />;
-    case 'Instagram':
-      return <IconInstagram />;
-    case 'Twitter':
-      return <IconTwitter />;
-    case 'External':
-      return <IconExternal />;
     case 'AppStore':
       return <IconAppStore />;
+    case 'Codepen':
+      return <IconCodepen />;
+    case 'External':
+      return <IconExternal />;
+    case 'Folder':
+      return <IconFolder />;
+    case 'Fork':
+      return <IconFork />;
+    case 'GitHub':
+      return <IconGitHub />;
+    case 'Instagram':
+      return <IconInstagram />;
+    case 'Linkedin':
+      return <IconLinkedin />;
+    case 'Loader':
+      return <IconLoader />;
+    case 'Location':
+      return <IconLocation />;
+    case 'Logo':
+      return <IconLogo />;
     case 'PlayStore':
       return <IconPlayStore />;
+    case 'Star':
+      return <IconStar />;
+    case 'Twitter':
+      return <IconTwitter />;
+    case 'Zap':
+      return <IconZap />;
     default:
       return <IconExternal />;
   }
