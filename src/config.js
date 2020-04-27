d491a6df01a99b69f7f01429657c7e483d7e3261
Shiyan Boxer <shiyanboxer7@gmail.com>
2020-04-26 20:19:31 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Shiyan Boxer | Software Engineer',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Shiyan Boxer is a software engineer based in Toronto, ON enjoys building websites, applications, and everything in between.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Shiyan Boxer, software engineer, computer engineering, intern, developer, university of waterloo, queens university',
+  siteUrl: 'https://shiyanboxer.netlify.app/',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Shiyan Boxer',
+  location: 'Toronto, ON',
+  email: 'shiyanboxer7@gmail.com',
+  github: 'https://github.com/shiyanboxer',
+  twitterHandle: '@shiyan_boxer',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/shiyanboxer',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/shiyanboxer',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/shiyanboxer',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/shiyan.boxer',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/shiyan_boxer',
     },
   ],
 
@@ -43,11 +43,11 @@ module.exports = {
     },
     {
       name: 'Experience',
-      url: '/#jobs',
+      url: '/#Experience',
     },
     {
       name: 'Work',
-      url: '/#projects',
+      url: '/#Work',
     },
     {
       name: 'Contact',
@@ -60,7 +60,7 @@ module.exports = {
   colors: {
     green: '#64ffda',
     navy: '#0a192f',
-    darkNavy: '#020c1b',
+    black: '#000000',
   },
 
   srConfig: (delay = 200) => ({
