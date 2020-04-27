d9d08e3ef5eea0f6c4e61f7772dcf62516e5d2dd
Shiyan Boxer <shiyanboxer7@gmail.com>
2020-04-27 18:18:36 -0400

    colors

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -31,7 +31,7 @@ const mixins = {
     &:hover,
     &:active,
     &:focus {
-      color: ${colors.green};
+      color: ${colors.black};
       outline: 0;
     }
   `,
@@ -43,17 +43,17 @@ const mixins = {
     position: relative;
     transition: ${theme.transition};
     cursor: pointer;
-    color: ${colors.green};
+    color: ${colors.black};
     &:hover,
     &:focus,
     &:active {
-      color: ${colors.green};
+      color: ${colors.black};
       outline: 0;
       &:after {
         width: 100%;
       }
       & > * {
-        color: ${colors.green} !important;
+        color: ${colors.black} !important;
         transition: ${theme.transition};
       }
     }
@@ -64,16 +64,16 @@ const mixins = {
       height: 1px;
       position: relative;
       bottom: 0.37em;
-      background-color: ${colors.green};
+      background-color: ${colors.black};
       transition: ${theme.transition};
       opacity: 0.5;
     }
   `,
 
   smallButton: css`
-    color: ${colors.green};
+    color: ${colors.black};
     background-color: transparent;
-    border: 1px solid ${colors.green};
+    border: 1px solid ${colors.black};
     border-radius: ${theme.borderRadius};
     padding: 0.75rem 1rem;
     font-size: ${fontSizes.smish};
@@ -85,7 +85,7 @@ const mixins = {
     &:hover,
     &:focus,
     &:active {
-      background-color: ${colors.transGreen};
+      background-color: ${colors.black};
     }
     &:after {
       display: none !important;
@@ -93,9 +93,9 @@ const mixins = {
   `,
 
   bigButton: css`
-    color: ${colors.green};
+    color: ${colors.black};
     background-color: transparent;
-    border: 1px solid ${colors.green};
+    border: 1px solid ${colors.black};
     border-radius: ${theme.borderRadius};
     padding: 1.25rem 1.75rem;
     font-size: ${fontSizes.sm};
@@ -107,7 +107,7 @@ const mixins = {
     &:hover,
     &:focus,
     &:active {
-      background-color: ${colors.transGreen};
+      background-color: ${colors.black};
     }
     &:after {
       display: none !important;
@@ -122,12 +122,12 @@ const mixins = {
   `,
 
   boxShadow: css`
-    box-shadow: 0 10px 30px -15px ${colors.shadowNavy};
+    box-shadow: 0 10px 30px -15px ${colors.black};
     transition: ${theme.transition};
 
     &:hover,
     &:focus {
-      box-shadow: 0 20px 30px -15px ${colors.shadowNavy};
+      box-shadow: 0 20px 30px -15px ${colors.black};
     }
   `,
 
@@ -144,7 +144,7 @@ const mixins = {
         content: '▹';
         position: absolute;
         left: 0;
-        color: ${colors.green};
+        color: ${colors.black};
       }
     }
   `,
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,22 +1,14 @@
 import { hex2rgba } from '@utils';
 
-const ACCENT = '#000000';
-const DARK_BG = '#020c1b';
-const BG = '#0a192f';
+const ACCENT = '#000000'; // black
+const DARK_BG = '#ffffff'; // white
+const BG = '#ffffff'; // white
 
 const theme = {
   colors: {
-    darkNavy: DARK_BG,
-    navy: BG,
-    lightNavy: '#172a45',
-    lightestNavy: '#303C55',
-    slate: '#8892b0',
-    lightSlate: '#a8b2d1',
-    lightestSlate: '#ccd6f6',
-    white: '#e6f1ff',
-    green: ACCENT,
-    transGreen: hex2rgba(ACCENT, 0.07),
-    shadowNavy: hex2rgba(DARK_BG, 0.7),
+    white: '#ffffff',
+    black: '#000000',
+    blue: '#0000ff',
   },
 
   fonts: {
