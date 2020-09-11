5527319afaa2b1da1d6750e3906d083e245530a7
Brittany Chiang <brittany.chiang@gmail.com>
2020-09-10 22:23:29 -0400

    Tweak big heading clamp

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -126,13 +126,13 @@ const GlobalStyle = createGlobalStyle`
 
   .big-heading {
     margin: 0;
-    font-size: clamp(40px, 10vw, 80px);
+    font-size: clamp(40px, 8vw, 80px);
     line-height: 1;
   }
 
   .medium-heading {
     margin: 0;
-    font-size: clamp(40px, 5vw, 60px);
+    font-size: clamp(40px, 8vw, 60px);
   }
 
   .numbered-heading {
