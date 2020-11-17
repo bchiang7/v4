6d84a66996a625fdf6594b34cf18fae71aa74bf9
Sambit Panda <36676569+sampan501@users.noreply.github.com>
2020-11-16 20:01:55 -0500

    Update work.js

--- src/components/sections/work.js
+++ src/components/sections/work.js
@@ -77,9 +77,9 @@ const StyledTabButton = styled.button`
   border-left: 0;
   text-align: center;
   white-space: nowrap;
-  font-size: ${fontSizes.h3};
-  ${media.tablet`font-size: 24px;`};
-  font-weight: 600;
+  font-size: ${fontSizes.h1};
+  // ${media.tablet`font-size: 24px;`};
+  // font-weight: 600;
   color: ${props => (props.isActive ? colors.green : colors.lightGrey)};
   border-bottom: 3px solid ${props => (props.isActive ? colors.green : colors.navy)};
   ${media.tablet`padding: 0 15px 2px; border-bottom: 2px solid ${props => (props.isActive ? colors.green : colors.navy)};`};
