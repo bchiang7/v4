dfb02a83b384b788d77b1f3059367b7f2954430c
Sambit Panda <36676569+sampan501@users.noreply.github.com>
2020-11-17 08:03:34 -0500

    fix profile pic weird issues

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -101,7 +101,7 @@ const StyledAvatarLink = styled.a`
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: ${colors.navy};
+    background-color: ${colors.white};
     mix-blend-mode: screen;
   }
   &:after {
