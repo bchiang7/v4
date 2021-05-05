cec1b9a954baff358b23a8040f6934ac687003fa
bchiang7 <brittany.chiang@gmail.com>
2021-05-05 12:06:05 -0400

    Bump node version to 14.16.0 and fix build

--- gatsby-node.js
+++ gatsby-node.js
@@ -69,7 +69,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 // https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
-  if (stage === 'build-html') {
+  if (stage === 'build-html' || stage === 'develop-html') {
     actions.setWebpackConfig({
       module: {
         rules: [
@@ -79,6 +79,10 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
           },
           {
             test: /animejs/,
+            use: loaders.null(),
+          },
+          {
+            test: /miniraf/,
             use: loaders.null(),
           },
         ],
