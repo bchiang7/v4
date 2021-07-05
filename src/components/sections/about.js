eae015d28eadce02f908c370694c8d014e80c39c
rbibart <77192870+rbibart@users.noreply.github.com>
2021-07-05 13:29:59 +0300

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -164,7 +164,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/IMG_0922.jpeg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
