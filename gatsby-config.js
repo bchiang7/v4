cda2711790e96295b16a9eb5ee80f34784f2d505
rbibart <77192870+rbibart@users.noreply.github.com>
2021-07-06 10:09:47 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Răzvan Bibarț',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Răzvan Bibarț is a security engineer',
     siteUrl: 'https://razvanbibart.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bibartr',
