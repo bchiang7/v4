70708ba52603a617e0fd63163def00776b2e66a7
rbibart <77192870+rbibart@users.noreply.github.com>
2021-07-06 22:06:53 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = ({ location }) => (
     <StyledMainContainer className="fillHeight">
       <Hero />
       <About />
+      <Jobs />
       <Featured />
       <Contact />    
     </StyledMainContainer>
