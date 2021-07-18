8ceb254b38c85a30116828e470ae4e5432483f34
Brittany Chiang <brittany.chiang@gmail.com>
2021-07-17 21:46:42 -0400

    Remove gatsby node stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,104 +4,104 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const path = require('path');
-const _ = require('lodash');
+// const path = require('path');
+// const _ = require('lodash');
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions;
-  const postTemplate = path.resolve(`src/templates/post.js`);
-  const tagTemplate = path.resolve('src/templates/tag.js');
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions;
+//   const postTemplate = path.resolve(`src/templates/post.js`);
+//   const tagTemplate = path.resolve('src/templates/tag.js');
 
-  const result = await graphql(`
-    {
-      postsRemark: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/posts/" } }
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
-          }
-        }
-      }
-      tagsGroup: allMarkdownRemark(limit: 2000) {
-        group(field: frontmatter___tags) {
-          fieldValue
-        }
-      }
-    }
-  `);
+//   const result = await graphql(`
+//     {
+//       postsRemark: allMarkdownRemark(
+//         filter: { fileAbsolutePath: { regex: "/posts/" } }
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//       tagsGroup: allMarkdownRemark(limit: 2000) {
+//         group(field: frontmatter___tags) {
+//           fieldValue
+//         }
+//       }
+//     }
+//   `);
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`);
-    return;
-  }
+//   // Handle errors
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`);
+//     return;
+//   }
 
-  // Create post detail pages
-  const posts = result.data.postsRemark.edges;
+//   // Create post detail pages
+//   const posts = result.data.postsRemark.edges;
 
-  posts.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.slug,
-      component: postTemplate,
-      context: {},
-    });
-  });
+//   posts.forEach(({ node }) => {
+//     createPage({
+//       path: node.frontmatter.slug,
+//       component: postTemplate,
+//       context: {},
+//     });
+//   });
 
-  // Extract tag data from query
-  const tags = result.data.tagsGroup.group;
-  // Make tag pages
-  tags.forEach(tag => {
-    createPage({
-      path: `/pensieve/tags/${_.kebabCase(tag.fieldValue)}/`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
-    });
-  });
-};
+//   // Extract tag data from query
+//   const tags = result.data.tagsGroup.group;
+//   // Make tag pages
+//   tags.forEach(tag => {
+//     createPage({
+//       path: `/pensieve/tags/${_.kebabCase(tag.fieldValue)}/`,
+//       component: tagTemplate,
+//       context: {
+//         tag: tag.fieldValue,
+//       },
+//     });
+//   });
+// };
 
-// https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
-  if (stage === 'build-html' || stage === 'develop-html') {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /scrollreveal/,
-            use: loaders.null(),
-          },
-          {
-            test: /animejs/,
-            use: loaders.null(),
-          },
-          {
-            test: /miniraf/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  }
+// // https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
+//   if (stage === 'build-html' || stage === 'develop-html') {
+//     actions.setWebpackConfig({
+//       module: {
+//         rules: [
+//           {
+//             test: /scrollreveal/,
+//             use: loaders.null(),
+//           },
+//           {
+//             test: /animejs/,
+//             use: loaders.null(),
+//           },
+//           {
+//             test: /miniraf/,
+//             use: loaders.null(),
+//           },
+//         ],
+//       },
+//     });
+//   }
 
-  actions.setWebpackConfig({
-    resolve: {
-      alias: {
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@config': path.resolve(__dirname, 'src/config'),
-        '@fonts': path.resolve(__dirname, 'src/fonts'),
-        '@hooks': path.resolve(__dirname, 'src/hooks'),
-        '@images': path.resolve(__dirname, 'src/images'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
-        '@styles': path.resolve(__dirname, 'src/styles'),
-        '@utils': path.resolve(__dirname, 'src/utils'),
-      },
-    },
-  });
-};
+//   actions.setWebpackConfig({
+//     resolve: {
+//       alias: {
+//         '@components': path.resolve(__dirname, 'src/components'),
+//         '@config': path.resolve(__dirname, 'src/config'),
+//         '@fonts': path.resolve(__dirname, 'src/fonts'),
+//         '@hooks': path.resolve(__dirname, 'src/hooks'),
+//         '@images': path.resolve(__dirname, 'src/images'),
+//         '@pages': path.resolve(__dirname, 'src/pages'),
+//         '@styles': path.resolve(__dirname, 'src/styles'),
+//         '@utils': path.resolve(__dirname, 'src/utils'),
+//       },
+//     },
+//   });
+// };
