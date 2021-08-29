cdd685d1ad926821c5f059b02646405a7c74c41d
Ayush Gupta <AyushG3112@gmail.com>
2021-08-29 23:19:54 +0530

    Update GA Tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,7 +152,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'UA-114716855-1',
       },
     },
   ],
