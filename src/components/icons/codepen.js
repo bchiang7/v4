1e7e1e89d8ff4d2e23e10d3c136f34cf39092128
rbibart <77192870+rbibart@users.noreply.github.com>
2021-09-20 09:13:13 +0300

    Update codepen.js

--- src/components/icons/codepen.js
+++ src/components/icons/codepen.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IconCodepen = () => (
   <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 31.665 31.665">
-    <title>Codepen</title>
+    <title>Facebook</title>
     <g transform="translate(-13.000000, -5.000000)">
     <path
       d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"
