fb8f61f70756dd2390c13cae857ca9bfcf169faf
rbibart <77192870+rbibart@users.noreply.github.com>
2021-09-20 09:04:48 +0300

    Update codepen.js

--- src/components/icons/codepen.js
+++ src/components/icons/codepen.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
 const IconCodepen = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    role="img"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="feather feather-codepen">
-    <title>CodePen</title>
-    <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
-    <line x1="12" y1="22" x2="12" y2="15.5"></line>
-    <polyline points="22 8.5 12 15.5 2 8.5"></polyline>
-    <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
-    <line x1="12" y1="2" x2="12" y2="8.5"></line>
+  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 31.665 31.665">
+    <title>Codepen</title>
+    <g transform="translate(-13.000000, -5.000000)">
+    <path
+      d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"
+          />
+          </g>
   </svg>
 );
 
