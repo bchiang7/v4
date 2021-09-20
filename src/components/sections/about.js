5b3230c4c28b4c28a631604e7c282189134131be
rbibart <77192870+rbibart@users.noreply.github.com>
2021-09-20 09:25:56 +0300

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -61,7 +61,7 @@ const StyledPic = styled.div`
     position: relative;
     width: 100%;
     border-radius: var(--border-radius);
-    background-color: var(--green);
+    background-color: var(--white);
 
     &:hover,
     &:focus {
