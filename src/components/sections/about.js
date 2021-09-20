6d282a6de4188c6b20e10975889afbe0d9558afc
rbibart <77192870+rbibart@users.noreply.github.com>
2021-09-20 09:33:26 +0300

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -83,7 +83,7 @@ const StyledPic = styled.div`
       position: relative;
       border-radius: var(--border-radius);
       mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
+     /* filter: grayscale(100%) contrast(1);*/
       transition: var(--transition);
     }
 
