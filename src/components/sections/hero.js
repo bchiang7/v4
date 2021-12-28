0b6240395f6c8ab1495c6be2b357cd3e80c579e4
Niyaskmgd <niyaskmgd@gmail.com>
2021-12-29 02:34:16 +0530

    2.34

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,12 +59,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Muhammad Niyas.</h2>;
+  const three = <h3 className="big-heading">Perfomance Marketing Enthusiast.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
+        I’m a Digital Marketing Guy specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
