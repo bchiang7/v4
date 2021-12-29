30fb680597401930bb360a3a3e3642dba3ad0508
Niyaskmgd <niyaskmgd@gmail.com>
2021-12-29 13:26:43 +0530

    ok

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -168,7 +168,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.png"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
