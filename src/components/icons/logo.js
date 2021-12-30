23b5ba78a3df68964129cff95790affcf58fd2d5
Niyaskmgd <niyaskmgd@gmail.com>
2021-12-31 00:14:37 +0530

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100.000000 100.000000" preserveAspectRatio="xMidYMid meet">
-
-<g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
-<path d="M270 871 l-225 -128 0 -243 0 -243 225 -128 c124 -71 228 -129 231
--129 4 0 107 58 230 128 l224 128 0 244 0 244 -224 128 c-123 70 -226 128
--230 128 -3 0 -107 -58 -231 -129z m170 -232 c0 -17 123 -228 133 -229 5 0 7
-54 5 120 l-3 120 40 0 40 0 0 -200 0 -200 -47 0 c-28 0 -48 5 -48 11 0 14
--119 217 -132 225 -6 4 -9 -36 -6 -115 l3 -121 -40 0 -40 0 0 200 0 200 48 0
-c29 0 47 -4 47 -11z" class="svg-elem-1"></path>
-</g>
+  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 612 792" enable-background="new 0 0 612 792" xml:space="preserve">
+<polygon fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" points="379,511 232.515,510.72 159.515,383.72 233,257 
+	379.485,257.28 452.485,384.28 " class="svg-elem-1"></polygon>
+<text transform="matrix(1 0 0 1 266 422)" font-family="'MyriadPro-Regular'" font-size="110">N</text>
 </svg>
 
 );
