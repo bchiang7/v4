2c57f319cd5a0f7f4ac8704f1378fa12e81df2ca
az0mb13 <qwertyuiop.dixit3@gmail.com>
2022-02-05 20:08:23 +0530

    Edited index

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,18 +59,17 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Aditya Dixit.</h2>;
+  const three = <h3 className="big-heading">I hack the internet.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I'm a Security Researcher and Penetration Tester specializing in breaking the security of 
+        websites, Android and iOS applications, internal and external networks, and cloud environments.
+        I'm currently working at {' '} as a Lead Security Researcher on Solidity, Smart Contracts, and Security on Blockchain. 
+        <a href="https://credshields.com/" target="_blank" rel="noreferrer">
+          CredShields
         </a>
-        .
       </p>
     </>
   );
