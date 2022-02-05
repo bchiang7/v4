ac191beca3b1bf8badf6b3be71f5ef172f4a027f
az0mb13 <qwertyuiop.dixit3@gmail.com>
2022-02-05 19:59:54 +0530

    modified gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Aditya Dixit',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Aditya Dixit is a Security Researcher and Penetration Tester who specializes in pentesting Web and Mobile Applications, Networks and Cloud Environments.',
+    siteUrl: 'https://dixitaditya.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@zombie007o',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'AdityaDixit',
+        short_name: 'AdityaDixit',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
