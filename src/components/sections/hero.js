bf97320fbdf7fa827c5aab509588d8636b694462
az0mb13 <qwertyuiop.dixit3@gmail.com>
2022-02-05 23:57:56 +0530

    index changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -75,10 +75,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://blog.dixitaditya.com"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my Blog!
     </a>
   );
 
