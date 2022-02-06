9c1ac7bc817d4d4af939c5d556fb85006fbd1c90
az0mb13 <qwertyuiop.dixit3@gmail.com>
2022-02-06 14:23:14 +0530

    testing footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -68,66 +68,66 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+  // const [githubInfo, setGitHubInfo] = useState({
+  //   stars: null,
+  //   forks: null,
+  // });
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV !== 'production') {
+  //     return;
+  //   }
+  //   fetch('https://api.github.com/repos/bchiang7/v4')
+  //     .then(response => response.json())
+  //     .then(json => {
+  //       const { stargazers_count, forks_count } = json;
+  //       setGitHubInfo({
+  //         stars: stargazers_count,
+  //         forks: forks_count,
+  //       });
+  //     })
+  //     .catch(e => console.error(e));
+  // }, []);
 
-  return (
-    <StyledFooter>
-      <StyledSocialLinks>
-        <ul>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <a href={url} aria-label={name}>
-                  <Icon name={name} />
-                </a>
-              </li>
-            ))}
-        </ul>
-      </StyledSocialLinks>
+  // return (
+  //   <StyledFooter>
+  //     <StyledSocialLinks>
+  //       <ul>
+  //         {socialMedia &&
+  //           socialMedia.map(({ name, url }, i) => (
+  //             <li key={i}>
+  //               <a href={url} aria-label={name}>
+  //                 <Icon name={name} />
+  //               </a>
+  //             </li>
+  //           ))}
+  //       </ul>
+  //     </StyledSocialLinks>
 
-      <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+  //     <StyledCredit tabindex="-1">
+  //       <a href="https://github.com/bchiang7/v4">
+  //         <div>Designed &amp; Built by Brittany Chiang</div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
-        </a>
-      </StyledCredit>
-    </StyledFooter>
-  );
+  //         {githubInfo.stars && githubInfo.forks && (
+  //           <div className="github-stats">
+  //             <span>
+  //               <Icon name="Star" />
+  //               <span>{githubInfo.stars.toLocaleString()}</span>
+  //             </span>
+  //             <span>
+  //               <Icon name="Fork" />
+  //               <span>{githubInfo.forks.toLocaleString()}</span>
+  //             </span>
+  //           </div>
+  //         )}
+  //       </a>
+  //     </StyledCredit>
+  //   </StyledFooter>
+  // );
 };
 
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+// Footer.propTypes = {
+//   githubInfo: PropTypes.object,
+// };
 
 export default Footer;
