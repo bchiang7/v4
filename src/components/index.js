17a3aa04965891d87361dc55c64a5baa43fca822
az0mb13 <qwertyuiop.dixit3@gmail.com>
2022-02-06 14:20:54 +0530

    testing footer

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ export { default as Menu } from './menu';
 export { default as Side } from './side';
 export { default as Social } from './social';
 export { default as Email } from './email';
-export { default as Footer } from './footer';
+// export { default as Footer } from './footer';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 export { default as Jobs } from './sections/jobs';
