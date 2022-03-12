65ef5e3e1c0aabef6adb06c9f4b1438aea8d8afd
Tayfun <tayfun.ceylan@outlook.de>
2022-03-12 19:27:33 +0100

    added Xing Icon

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -15,6 +15,7 @@ import {
   IconPlayStore,
   IconStar,
   IconTwitter,
+  IconXing,
 } from '@components/icons';
 
 const Icon = ({ name }) => {
@@ -47,6 +48,8 @@ const Icon = ({ name }) => {
       return <IconStar />;
     case 'Twitter':
       return <IconTwitter />;
+    case 'Xing':
+      return <IconXing />;
     default:
       return <IconExternal />;
   }
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -13,3 +13,4 @@ export { default as IconLogo } from './logo';
 export { default as IconPlayStore } from './playstore';
 export { default as IconStar } from './star';
 export { default as IconTwitter } from './twitter';
+export { default as IconXing } from './xing';
