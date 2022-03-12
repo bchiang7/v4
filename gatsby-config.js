23f8f2472e9ab32aad9e1e4c14ea0cf7dd78971c
Tayfun <tayfun.ceylan@outlook.de>
2022-03-12 19:30:11 +0100

    changed texts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Tayfun Ceylan',
-    description:
-      'Tayfun Ceylan is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    description: 'Tayfun Ceylan is an IT student specializing in cryptography.',
+    siteUrl: 'https://tydn.de', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'TayfunCeylan',
+        short_name: 'TayfunCeylan',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
