de5ad6da1e56614104388f26fb2239ac61df9bab
Hichem Fantar <hichemfantar2049@gmail.com>
2022-07-09 00:11:26 +0100

    reduce chances of colliding with absolute path

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     {
       postsRemark: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/posts/" } }
+        filter: { fileAbsolutePath: { regex: "/content/posts/" } }
         sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
       ) {
--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -170,7 +170,7 @@ const Projects = () => {
     query {
       projects: allMarkdownRemark(
         filter: {
-          fileAbsolutePath: { regex: "/projects/" }
+          fileAbsolutePath: { regex: "/content/projects/" }
           frontmatter: { showInProjects: { ne: false } }
         }
         sort: { fields: [frontmatter___date], order: DESC }
