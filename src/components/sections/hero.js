0be934b92c3c73cddcb2c1481af4420d29d22fa3
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-10-24 18:11:07 +0100

    feat: added hero information

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,18 +59,13 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Emmanuel Eze.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
         I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        digital experiences. Currently, I enjoy building accessible, human-centered products.
       </p>
     </>
   );
@@ -79,7 +74,8 @@ const Hero = () => {
       className="email-link"
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
-      rel="noreferrer">
+      rel="noreferrer"
+    >
       Check out my course!
     </a>
   );
