5c9bec8174c4c7b15b47bf2620828a3491dc6489
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-10-27 23:51:54 +0100

    feat: added about text

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,32 +135,13 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              I'm a full-stack JavaScript software engineer that enjoys building and scaling both
+              server and client-side applications using test-driven development processes, often
+              working remotely in distributed Agile software development teams. I focus primarily on
+              delivering clean code that is easy to scale and maintain. Effective communication is
+              an essential tool l use for setting clear expectations with my clients. You will find
+              in me an accountable, solid partner for your business.
             </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
 
