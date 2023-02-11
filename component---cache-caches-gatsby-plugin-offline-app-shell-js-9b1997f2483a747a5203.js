78e3d96eb8168d2b0588083c7eb75b1d6ff7f152
Nikhil <70238901+Stratonov16@users.noreply.github.com>
2023-02-12 01:18:00 +0530

    Build files

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkv4=self.webpackChunkv4||[]).push([[306],{1930:function(e,t,n){var u=n(4836);t.__esModule=!0,t.default=void 0;var r=u(n(7867)),a=u(n(7294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js.map
