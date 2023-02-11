331cef8aa4ecabef82843100ad6bc578f6aab8a3
Nikhil <stratonov16@gmail.com>
2023-02-11 17:18:53 +0530

    updated about

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,9 +136,7 @@ const About = () => {
           <div>
             <p>
               Hello! My name is Nikhil Sachdeva and I enjoy creating things that live on the
-              internet. My interest in web development started back in 2012 when I decided to try
-              editing custom Tumblr themes — turns out hacking together a custom reblog button
-              taught me a lot about HTML &amp; CSS!
+              internet.
             </p>
 
             <p>
