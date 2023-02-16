f5c57401e26a442dc157252369621a7e8b8860a1
MaxVzl <maxime.vozelle@gmail.com>
2023-02-16 20:11:51 +0100

    send

--- src/components/menu.js
+++ src/components/menu.js
@@ -267,7 +267,7 @@ const Menu = () => {
             )}
 
             <a href="/resume.pdf" className="resume-link">
-              Mon CV
+              Mon CVe
             </a>
           </nav>
         </StyledSidebar>
