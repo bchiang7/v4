ff3f645803bf43136119a2a95000dd03144c14f1
masomaster <masonlancaster@protonmail.com>
2023-03-23 14:14:49 -0700

    removes unnecessary files/data

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,13 +44,13 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/content/posts`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `posts`,
+    //     path: `${__dirname}/content/posts`,
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
