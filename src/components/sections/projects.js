7ef4b5bb4931235de7f3d66b7cdaca1de68521bc
masomaster <masonlancaster@protonmail.com>
2023-03-22 17:15:57 -0700

    restyles 'other' projects

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -27,7 +27,7 @@ const StyledProjectsSection = styled.section`
   .projects-grid {
     ${({ theme }) => theme.mixins.resetList};
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
     grid-gap: 15px;
     position: relative;
     margin-top: 50px;
