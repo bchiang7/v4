fa6a508b0c62b56179bd51c07936c795051ee2f7
masomaster <masonlancaster@protonmail.com>
2023-03-23 13:59:19 -0700

    removes unnecessary paths

--- content/featured/Moment/index.md
+++ content/featured/Moment/index.md
@@ -3,14 +3,12 @@ date: '4'
 title: 'Moment: A San Diego Date Planner'
 cover: './moment.png'
 github: 'https://github.com/masomaster/Moment-date-planner'
-external: ''
+external: 'https://moment-sd.vercel.app/'
 tech:
-  - Node.js
-  - Express.js
-  - EJS
-  - MongoDB
-  - Mongoose
+  - Node/Express
+  - MongoDB/Mongoose
   - Google OAuth
+  - Vercel
 ---
 
 Ever been at a loss for what do on a date? Moment to the rescue! Users can create and categorize public or private activities then compile them into a full public or private date plan using full CRUD operations via RESTful routing.
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,66 +5,66 @@
  */
 
 const path = require('path');
-const _ = require('lodash');
+// const _ = require('lodash');
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions;
-  const postTemplate = path.resolve(`src/templates/post.js`);
-  const tagTemplate = path.resolve('src/templates/tag.js');
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions;
+//   const postTemplate = path.resolve(`src/templates/post.js`);
+//   const tagTemplate = path.resolve('src/templates/tag.js');
 
-  const result = await graphql(`
-    {
-      postsRemark: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/content/posts/" } }
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
-          }
-        }
-      }
-      tagsGroup: allMarkdownRemark(limit: 2000) {
-        group(field: frontmatter___tags) {
-          fieldValue
-        }
-      }
-    }
-  `);
+//   const result = await graphql(`
+//     {
+//       postsRemark: allMarkdownRemark(
+//         filter: { fileAbsolutePath: { regex: "/content/posts/" } }
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//       tagsGroup: allMarkdownRemark(limit: 2000) {
+//         group(field: frontmatter___tags) {
+//           fieldValue
+//         }
+//       }
+//     }
+//   `);
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`);
-    return;
-  }
+//   // Handle errors
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`);
+//     return;
+//   }
 
-  // Create post detail pages
-  const posts = result.data.postsRemark.edges;
+//   // Create post detail pages
+//   const posts = result.data.postsRemark.edges;
 
-  posts.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.slug,
-      component: postTemplate,
-      context: {},
-    });
-  });
+//   posts.forEach(({ node }) => {
+//     createPage({
+//       path: node.frontmatter.slug,
+//       component: postTemplate,
+//       context: {},
+//     });
+//   });
 
-  // Extract tag data from query
-  const tags = result.data.tagsGroup.group;
-  // Make tag pages
-  tags.forEach(tag => {
-    createPage({
-      path: `/pensieve/tags/${_.kebabCase(tag.fieldValue)}/`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
-    });
-  });
-};
+//   // Extract tag data from query
+//   const tags = result.data.tagsGroup.group;
+//   // Make tag pages
+//   tags.forEach(tag => {
+//     createPage({
+//       path: `/pensieve/tags/${_.kebabCase(tag.fieldValue)}/`,
+//       component: tagTemplate,
+//       context: {
+//         tag: tag.fieldValue,
+//       },
+//     });
+//   });
+// };
 
 // https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
