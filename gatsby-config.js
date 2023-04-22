546e96503203a081d4c71bdec24d730a701ab3bf
akolate <aniketkolte18@gmail.com>
2023-04-23 01:33:15 +0530

    Added Google Analytics Tracking Id

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,7 +152,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'G-QTWK9R1JFT',
       },
     },
   ],
