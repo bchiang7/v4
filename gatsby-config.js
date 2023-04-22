8edd4f72f3153d1900d89fea6c4b6dfd662cb256
akolate <aniketkolte@gmail.com>
2023-04-23 00:09:35 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Portfolio | Aniket Kolte',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Aniket Kolte is a software engineer who loves to develop high-quality and robust web applications.',
+    siteUrl: 'https://aniketkolte.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Aniket Kolte',
+        short_name: 'Aniket Kolte',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
