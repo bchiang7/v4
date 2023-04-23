a0072a94ad5de99fc9dacfd45e6c5d6628049bba
akolate <aniketkolte18@gmail.com>
2023-04-23 13:35:39 +0530

    Minor design fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import ProgressBar from 'react-scroll-progress-bar';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -9,6 +10,7 @@ const StyledMainContainer = styled.main`
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
+    <ProgressBar bgcolor="#64ffda" />
     <StyledMainContainer className="fillHeight">
       <Hero />
       <About />
