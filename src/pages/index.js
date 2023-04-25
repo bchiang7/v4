2ad8f85553d8dacfd3bbc0afa962d915c6ba652e
Josh <j.p.lu@outlook.com>
2023-04-25 23:14:27 +1000

    fix: try fixing analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,26 @@ import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Contact } from '@components';
 import { inject } from '@vercel/analytics';
 
-inject();
-
 const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
-const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      {/* <Projects /> */}
-      <Contact />
-    </StyledMainContainer>
-  </Layout>
-);
+const IndexPage = ({ location }) => {
+  inject();
+
+  return (
+    <Layout location={location}>
+      <StyledMainContainer className="fillHeight">
+        <Hero />
+        <About />
+        <Jobs />
+        <Featured />
+        {/* <Projects /> */}
+        <Contact />
+      </StyledMainContainer>
+    </Layout>
+  );
+};
 
 IndexPage.propTypes = {
   location: PropTypes.object.isRequired,
