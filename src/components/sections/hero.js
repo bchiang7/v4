d13e8ce398ba3b8b6d2b306e8f1b7aa4b4795c5d
Mrigendra Soni <mrigendrasoni26@gmail.com>
2023-06-11 21:57:59 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Mrigendra Soni.</h2>;
+  const three = <h3 className="big-heading">Lets just say I hack things.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a product security engineer specializing in pentesting web apps and networks,
+        conducting cloud environment audits and local privilege escalation. Currently, I’m focused on securing the awesome products
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://zendesk.com/" target="_blank" rel="noreferrer">
+          Zendesk
         </a>
         .
       </p>
@@ -78,10 +78,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="blog.hexbyte.in"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my blog!
     </a>
   );
 
