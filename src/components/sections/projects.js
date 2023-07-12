f9fe048e8b7328999b737461202327c20f5c160d
Mrigendra Soni <mrigendrasoni26@gmail.com>
2023-07-12 08:41:24 +0530

    Update projects.js

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -264,7 +264,7 @@ const Projects = () => {
   };
    return (
     <StyledProjectsSection>
-      <h2 ref={revealTitle}>Other Noteworthy Projects</h2>
+      <h2 ref={revealTitle}>My Blogs and Articles</h2>
 
       <Link className="inline-link archive-link" to="/archive" ref={revealArchiveLink}>
         view the archive
