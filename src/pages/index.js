c7247419d6939dce16fb4fa0777faf0fef2ad4c2
AvaneeshKhandekar <avaneesh.khandekar@gmail.com>
2023-07-16 13:06:53 +0530

    Testing stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import { Layout, Hero, About, Jobs, Projectss } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
