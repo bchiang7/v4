33e13da0a8985d6758dcecf83746b2e15931c4d7
AvaneeshKhandekar <avaneesh.khandekar@gmail.com>
2023-07-16 13:59:06 +0530

    adding photo and logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Avaneesh Khandekar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Avaneesh Khandekar is a software engineer who specializes in building cloud solutions.',
+    siteUrl: 'https://avaneeshkhandekar.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '4v4neesh',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Avaneesh Khandekar',
+        short_name: 'Avaneesh Khandekar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
