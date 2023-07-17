08b607e9e72c921d15f05ccd6b69617b3b140cf5
AvaneeshKhandekar <avaneesh.khandekar@gmail.com>
2023-07-17 21:41:06 +0530

    Added work details

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -297,9 +297,9 @@ const Projects = () => {
         )}
       </ul>
 
-      <button className="more-button" onClick={() => setShowMore(!showMore)}>
+      <!-- <button className="more-button" onClick={() => setShowMore(!showMore)}>
         Show {showMore ? 'Less' : 'More'}
-      </button>
+      </button> -->
     </StyledProjectsSection>
   );
 };
