9e6d7783764c71611c020e942da388813b91fb16
AvaneeshKhandekar <avaneesh.khandekar@gmail.com>
2023-07-19 21:11:37 +0530

    Added personal details

--- src/components/footer.js
+++ src/components/footer.js
@@ -85,7 +85,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/AvaneeshKhandekar/personal-website">
           <div>Redesigned by AvaneeshKhandekar from bchiang7's v4</div>
         </a>
       </StyledCredit>
