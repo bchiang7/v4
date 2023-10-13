bad56453e8c8bd9741801fe9ba1ada9544492d77
nathandunn <nathan.dunn.aml@gmail.com>
2023-10-13 14:25:08 -0500

    Adjusts hero page

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,22 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Nathan Dunn.</h2>;
+  const three = <h3 className="big-heading">I make things work.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software engineer who focuses on front-end development for both web and mobile.
+        Currently, I build online marketplaces at{' '}
+        <a href="https://www.integratedauctionsolutions.com/" target="_blank" rel="noreferrer">
+          Integrated Auction Solutions
         </a>
         .
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
