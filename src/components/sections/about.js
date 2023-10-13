eb148abf9c6d874db609910e00d32837121f5fc0
nathandunn <nathan.dunn.aml@gmail.com>
2023-10-13 14:57:42 -0500

    Swaps profile pic

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -173,7 +173,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.png"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
