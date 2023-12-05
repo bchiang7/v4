c04e5385b57319eeb72cd1921a57e2aee91b5d66
Thad Avery <thadaver@colostate.edu>
2023-12-05 15:04:52 -0600

    first commit

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,8 +60,8 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Thad Avery</h2>;
+  const three = <h3 className="big-heading">I build things.</h3>;
   const four = (
     <>
       <p>
