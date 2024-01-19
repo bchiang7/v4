a3f6177f9aebc1419afda9dec774d5c45c18d853
Abhinav Ramesh <dbossgames20@gmail.com>
2024-01-19 10:51:07 -0500

    make changes to gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Abhinav Ramesh',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Abhinav Ramesh is a software engineer who specializes in Full Stack, ML/AI, and embedded programming.',
+    siteUrl: 'https://abhibyte.github.io/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Abhinav Ramesh',
+        short_name: 'Abhinav Ramesh',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
